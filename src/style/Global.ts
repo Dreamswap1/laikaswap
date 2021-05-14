d08fd973962af69144d3345a043e9e1ccec02499
Lalita <46801379+kesaviweb@users.noreply.github.com>
2021-05-14 09:59:34 +0530

    Updated Global Styles

--- src/style/Global.ts
+++ src/style/Global.ts
@@ -3,39 +3,38 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
   body {
     background: url(../images/egg/laikaswap-bg.jpg);
-
+    
     img {
       height: auto;
       max-width: 100%;
     }
-    #laika-btn {
-      background: #54bfde;
-    }
-    .iMuQQP .desktop-icon {
-      width: 380px!important;
-    }
-    .iMuQQP .mobile-icon {
-      width: 60px;
-    }
-    .bZryuS, .jqXSbG {
-      background-color: #54bfde!important;
-    }
-    .lctQjQ, .SWUvA, .cNpGDU, .eNOLL {
-      color: #54bfde;
-    }
-    .bRIDge {
-      display:none;
-    }
-    .gGTCcW, .SfwyR, .vzgrd svg {
-      fill: #54bfde;
-    }
-    @media only screen and (max-width: 960px) {
-      .gHbDCl {
-        margin-top:10px;
-      }
+  }
+  #laika-btn {
+    background: #54bfde;
+  }
+  .iMuQQP .desktop-icon, .jkkUee .desktop-icon {
+    width: 380px!important;
+  }
+  .iMuQQP .mobile-icon, .jkkUee .mobile-icon {
+    width: 60px;
+  }
+  .bZryuS, .jqXSbG, .bUcNKF, .btoybd {
+    background-color: #54bfde!important;
+  }
+  .lctQjQ, .SWUvA, .cNpGDU, .eNOLL, .xiYlH, .eIORCb, .ZkHOl, .DxMoL {
+    color: #54bfde;
+  }
+  .bRIDge, .eNpfYQ {
+    display:none;
+  }
+  .gGTCcW, .SfwyR, .vzgrd svg, .ewCGLh, .hDQNBz svg, .hDQNBz svg, .dRSIFi {
+    fill: #54bfde;
+  }
+  @media only screen and (max-width: 960px) {
+    .gHbDCl, .kbxvML {
+      margin-top:10px;
     }
   }
-
 `
 
 export default GlobalStyle
